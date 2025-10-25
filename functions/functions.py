885e2a374927984f0a9069d16f49e331d02ca9c9
denis <minka.denis@gmail.com>
2025-10-25 13:48:33 +0300

    init

---
+++ functions/functions.py
@@ -0,0 +1,15 @@
+
+def plus(a, b):
+    return a + b
+
+def minus(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    return a / b
+
+
+abc = "qwe"
---
+++ functions/main.py
@@ -0,0 +1,15 @@
+
+import functions
+
+
+r1 = functions.plus(1, 2)
+r2 = functions.minus(1, 2)
+r3 = functions.multiply(1, 2)
+r4 = functions.divide(1, 2)
+
+print(r1)
+print(r2)
+print(r3)
+print(r4)
+
+print(functions.abc)
