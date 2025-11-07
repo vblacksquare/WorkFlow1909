ee380698be549793dcd2181a88188ff1da67dfb1
denis <minka.denis@gmail.com>
2025-11-07 03:16:42 +0200

    init

--- rpg-functions/enemies.py
+++ rpg-functions/enemies.py
@@ -48,6 +48,82 @@ def create_bat():
     return goblin
 
 
+def create_baby_ogr():
+    ogr = create_person("Огр")
+    ogr["stats"]["health"] = 20
+
+    return ogr
+
+def create_big_ogr():
+    ogr = create_person("Великий огр")
+    ogr["stats"]["health"] = 50
+
+    return ogr
+
+def create_ogr_mage():
+    ogr = create_person("Огр маг")
+    ogr["stats"]["health"] = 30
+
+    return ogr
+
+def create_lord_of_ogrs_boss():
+    ogr = create_person("Принц Огрів")
+    ogr["stats"]["health"] = 250
+
+    return ogr
+
+def create_goblin_king_boss():
+    goblin = create_person("король гоблінів")
+    goblin["stats"]["health"] = 190
+    goblin["stats"]["level"] = 10
+
+    return goblin
+
+
+#: level 5
+def create_runic_skeleton():
+    s = create_person("Рунический скелет")
+    s["stats"]["health"] = 35
+    s["stats"]["strength"] = 10
+    s["stats"]["agility"] = 6
+    s["stats"]["intelligence"] = 8
+    s["stats"]["level"] = 7
+
+    return s
+
+def create_ruin_guardian():
+    g = create_person("Страж руїн")
+    g["stats"]["health"] = 45
+    g["stats"]["strength"] = 12
+    g["stats"]["agility"] = 4
+    g["stats"]["intelligence"] = 6
+    g["stats"]["level"] = 10
+
+    return g
+
+
+def create_ink_wolf():
+    wolf = create_person("Чорнильний вовк")
+    wolf["stats"]["health"] = 35
+    wolf["stats"]["strength"] = 6
+    wolf["stats"]["agility"] = 12
+    wolf["stats"]["intelligence"] = 2
+    wolf["stats"]["level"] = 4
+
+    return wolf
+
+
+def create_hell_boss():
+    b = create_person("Адський повелитель")
+    b["stats"]["health"] = 100
+    b["stats"]["strength"] = 25
+    b["stats"]["agility"] = 10
+    b["stats"]["intelligence"] = 14
+    b["stats"]["level"] = 25
+
+    return b
+
+
 #: hero
 def create_hero(nickname):
     hero = create_person(nickname)
--- rpg-functions/locations.py
+++ rpg-functions/locations.py
@@ -6,6 +6,28 @@ def location1():
     return {
         "name": "якась локація",
         "description": "пояснення",
-        "enemies": [enemies.create_fat_slime(), enemies.create_bat(), enemies.create_small_slime()],
+        "enemies": [enemies.create_fat_slime(), enemies.create_small_slime()],
         "boss": [enemies.create_magma_cube_boss()]
+    }
+
+
+def location2():
+    return {
+        "name": "Поселище гоблінів та огрів",
+        "description": "там живуть гобліни",
+        "enemies": [
+            enemies.create_strong_goblin(), enemies.create_weak_goblin(),
+            enemies.create_baby_ogr(), enemies.create_ogr_mage(),
+            enemies.create_big_ogr()
+        ],
+        "boss": [enemies.create_goblin_king_boss(), enemies.create_lord_of_ogrs_boss()]
+    }
+
+
+def location5():
+    return {
+        "name": "Пекло",
+        "description": "Саме пекло світу в якому... доволі спекотно",
+        "enemies": [enemies.create_runic_skeleton(), enemies.create_ruin_guardian(), enemies.create_ink_wolf()],
+        "boss": [enemies.create_hell_boss()]
     }
